d849e0c69ddb0a31c7ed3be3c424968fe2a01f5f
powcoder <powcoder@163.com>
2022-11-22 13:55:30 +0800

    CS61 Classify proj2 RISC V

---
+++ src/relu.s
@@ -0,0 +1,42 @@
+https://powcoder.com
+代写代考加微信 powcoder
+Assignment Project Exam Help
+Add WeChat powcoder
+.globl relu
+
+.text
+# ==============================================================================
+# FUNCTION: Performs an inplace element-wise ReLU on an array of ints
+# Arguments:
+# 	a0 (int*) is the pointer to the array
+#	a1 (int)  is the # of elements in the array
+# Returns:
+#	None
+#
+# If the length of the vector is less than 1, 
+# this function exits with error code 8.
+# ==============================================================================
+relu:
+    # Prologue
+
+
+loop_start:
+    
+
+
+
+
+
+
+
+loop_continue:
+
+
+
+loop_end:
+
+
+    # Epilogue
+
+    
+	ret
---
+++ tests/assembly/test_read_matrix.s
@@ -0,0 +1,21 @@
+https://powcoder.com
+代写代考加微信 powcoder
+Assignment Project Exam Help
+Add WeChat powcoder
+.import ../../src/read_matrix.s
+.import ../../src/utils.s
+
+.data
+file_path: .asciiz "inputs/test_read_matrix/test_input.bin"
+
+.text
+main:
+    # Read matrix into memory
+
+
+
+    # Print out elements of matrix
+
+
+
+    # Terminate the program
